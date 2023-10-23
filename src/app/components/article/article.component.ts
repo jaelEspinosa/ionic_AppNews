af7866eee176a668a26902e82434a11ce74b96e8
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2023-10-23 19:08:41 +0200

    add menu card share and browser

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,5 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { ActionSheetController, Platform } from '@ionic/angular';
 import { Article } from 'src/app/interfaces';
+import { Browser } from '@capacitor/browser';
+import { Share } from '@capacitor/share'
+import { Source } from '../../interfaces/news';
+
+
 
 @Component({
   selector: 'app-article',
@@ -11,8 +17,61 @@ export class ArticleComponent  implements OnInit {
   @Input() article!: Article
   @Input() i!: number
 
+
+  private platform = inject ( Platform )
+  private actionCtrl = inject ( ActionSheetController )
+
   constructor() { }
 
   ngOnInit() {}
 
+  async onOpenMenu(){
+
+    const actionSheet = await this.actionCtrl.create({
+      header:'Opciones',
+      mode:'ios',
+      cssClass:'myActSheet',
+      backdropDismiss: false,
+      buttons:[
+        {
+          text:'Compartir',
+          icon: 'share-outline',
+          handler:()=>this.onShareArticle()
+        },
+        {
+          text:'Favorito',
+          icon: 'heart-outline',
+          handler:()=>this.onToggleFavorite()
+        },
+        {
+          text:'Cancelar',
+          icon: 'close-outline',
+          role:'cancelar'
+        }
+      ]
+    })
+   await actionSheet.present()
+  }
+
+ async onShareArticle(){
+ const{ title, source, url} = this.article
+ await Share.share({
+  title: title,
+  text: title,
+  url,
+  dialogTitle: 'Shared by'+source.name,
+});
+  }
+
+  async openArticle() {
+    if(this.platform.is('ios') || this.platform.is('android')){
+      await Browser.open({ url: this.article.url });
+      return;
+    }
+   window.open( this.article.url, '_blank')
+  }
+
+  onToggleFavorite(){
+    console.log('Toggle Favorite')
+  }
 }
